69b2cb0ad205ec4df805cc6c1137963b70d1afc1
Sajidhgn1 <sajid.ali@tanbits.org>
2022-07-20 18:54:34 +0500

    procfile

--- config/prod.exs
+++ config/prod.exs
@@ -30,7 +30,7 @@ config :exauth, ExauthWeb.Endpoint,
 config :exauth, ExauthWeb.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "20"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
   #import_config "prod.secret.exs"
 #
